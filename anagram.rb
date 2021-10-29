cc7579cf7bdcd2b5bdebd7403b58528ebf767454
Anuj Gupta <apgupta3091@gmail.com>
2021-10-29 17:12:54 -0400

    started two_sum

--- anagram.rb
+++ anagram.rb
@@ -1,13 +1,20 @@
 def first_anagram?(first, second)
     arr = []
     first.chars.permutation.to_a.each_with_index {|chars,i| arr[i] = chars.join("")}
-    p first.include?(second)
+    first.include?(second)
 end
 
 def sec_anagram?(first, second)
-    arr = []
-    first.chars.permutation.to_a.each_with_index {|chars,i| arr[i] = chars.join("")}
-    p first.include?(second)
+    first.each_char do |char|
+        second_idx = second.chars.index(char)
+        second = second[0...second_idx] + second[second_idx + 1..-1] unless second_idx.nil?
+    end
+    return second.length == 0
+end
+
+def third_anagram?(first, second)
+    first.chars.sort == second.chars.sort
+    
 end
 
 def fourth_anagram?(first, second)
@@ -15,12 +22,20 @@ def fourth_anagram?(first, second)
     hash2 = Hash.new(0)
     first.chars.each {|char| hash1[char] += 1}
     second.chars.each {|char| hash2[char] += 1}
-    p hash1 == hash2
+    hash1 == hash2
 end
 
-first_anagram?("not", "yes") #=> false
-first_anagram?("gizmo", "sally") #=> false
-first_anagram?("elvis", "lives") #=> true
+# first_anagram?("not", "yes") #=> false
+# first_anagram?("gizmo", "sally") #=> false
+# first_anagram?("elvis", "lives") #=> true
+
+# p sec_anagram?("not", "yes") #=> false
+# p sec_anagram?("gizmo", "sally") #=> false
+# p sec_anagram?("elvis", "lives") #=> true
+
+# p third_anagram?("no", "yes") #=> false
+# p third_anagram?("gizmo", "sally") #=> false
+# p third_anagram?("elvis", "lives") #=> true
 
 
 #fourth_anagram?("gizmo", "sally") #=> false
---
+++ two_sum.rb
@@ -0,0 +1,20 @@
+
+def bad_two_sum?(arr, target_sum)
+    arr.each_with_index do |ele, i|
+        (i + 1...arr.length).each do |j|
+            if ele + arr[j] == target_sum
+                return true 
+            end
+        end
+    end
+    false 
+end
+
+arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+
+
+def okay_two_sum?(arr, target_sum)
+    
+end
