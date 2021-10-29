4d755b98956400c8c6f50baeaaeb3f01a81ac268
Anuj Gupta <apgupta3091@gmail.com>
2021-10-29 14:45:33 -0400

    finsihed phase 1 of subsum

--- exectime.rb
+++ exectime.rb
@@ -1,14 +1,6 @@
-def my_min(list)
-    min = list.first
-    (1...list.count).each do |i|
-        list.each_with_index do |ele, j|
-            if ele < list[i]
-                min = ele
-            end
-        end
-    end
-    min
-end
+# def my_min(list)
+    
+# end
 
 
 def my_min2(list)
@@ -18,5 +10,28 @@ def my_min2(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
-p my_min2(list)  # =>  -5
+# p my_min(list)  # =>  -5
+# p my_min2(list)  # =>  -5
+
+
+def largest_contiguous_subsum(list)
+    subarrays = []
+    list.each_with_index do |ele, i|
+         if i == list.length - 1
+            subarrays << [ele]
+            next 
+         end
+        (i...list.count).each do |j|
+            subarrays << list[i..j]
+        end
+    end
+    subarrays.map {|subarrays| subarrays.sum}.max
+end
+
+
+
+# list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
